2eb9bc88a7581c4033b7c74e6a0a15ab38e5e12d
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-08-14 21:03:33 +0100

    Bot]ao whatsapp ocultado da home

--- src/pages/site/Home.jsx
+++ src/pages/site/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
       <Start />
 
       {/* Botão WhatsApp fixo */}
-      <WhatsAppButton phone="244972614886" size={64} />
+      {/*<WhatsAppButton phone="244972614886" size={64} />*/}
     </>
   );
 }
