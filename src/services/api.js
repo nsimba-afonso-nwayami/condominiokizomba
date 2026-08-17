0a5a5d68e62386caf79778b57f9a552bb02a56d1
hossidev <nsimba2025nwayami@gmail.com>
2026-08-17 12:38:44 +0100

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -18,7 +18,7 @@ export const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true,
+  withCredentials: false,
 });
 
 /* ============================
